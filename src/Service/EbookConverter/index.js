b1120f69228d5544a2069c3e738f2aab57249ef7
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-01-15 20:53:15 -0300

    'then' and 'catch' approach replaced by 'async' and 'await'

--- src/Service/EbookConverter/index.js
+++ src/Service/EbookConverter/index.js
@@ -1,3 +1,19 @@
 const ebookConverter = require("node-ebook-converter");
 ebookConverter.setPoolSize(10);
 
+const EbookConverter = async (file_name) => {
+    console.log('Iniciando conversão');
+    await ebookConverter.convert({
+        input:`./books/${file_name}`,
+        output:`./books/${file_name.slice(0,-5)}.mobi`,
+        delete:true,
+        silent:true
+    });
+    console.log('Conversão finalizada');
+}
+
+
+
+module.exports = {
+    EbookConverter
+}
