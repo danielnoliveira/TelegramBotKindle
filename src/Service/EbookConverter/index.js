baca5f03773fc1e5a831ffea9655c32eb0d2569c
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-01-28 12:53:15 -0300

    function of converting ebooks changed: added del (for delete) and file_path parameters

--- src/Service/EbookConverter/index.js
+++ src/Service/EbookConverter/index.js
@@ -1,15 +1,22 @@
 const ebookConverter = require("node-ebook-converter");
 ebookConverter.setPoolSize(10);
 
-const EbookConverter = async (file_name) => {
-    console.log('Iniciando conversão');
-    await ebookConverter.convert({
-        input:`./books/${file_name}`,
-        output:`./books/${file_name.slice(0,-5)}.mobi`,
-        delete:true,
-        silent:true
-    });
-    console.log('Conversão finalizada');
+const EbookConverter = async (file_path,file_name,del=true) => {
+    try{
+        console.log('Iniciando conversão');
+        await ebookConverter.convert({
+            input:`${file_path}${file_name}`,
+            output:`${file_path}${file_name.slice(0,-5)}.mobi`,
+            delete:del,
+            silent:true
+        });
+        console.log('Conversão finalizada');
+        return 'success';
+    }catch(e){
+        console.log('Falha na conversão');
+        console.error(e);
+        return 'fail'
+    }
 }
 
 
