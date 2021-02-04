1943fa5bec6672b8aed34ac9e39db5271413fc31
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-02-04 12:38:45 -0300

    timeout limit modified

--- test/ebookConverter.test.js
+++ test/ebookConverter.test.js
@@ -1,5 +1,6 @@
 const calibre = require('./../src/Service/EbookConverter');
 
+jest.setTimeout(30000);
 test('Função que converte arquivo em formato epub para o formato mobi', () => {
     return calibre.EbookConverter('./../garbage/','trash.epub',false).then(data=>{
         expect(data).toBe('success');
