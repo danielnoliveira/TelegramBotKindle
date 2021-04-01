45643d29c558ee0bdc4b0a9c8ee45a7573898813
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-04-01 19:39:12 -0300

    shhhhhgit add .gitignore secrets.js git add .gitignore secrets.js

---
+++ secrets.js
@@ -0,0 +1,11 @@
+const TELEGRAM_TOKEN = '1570964767:AAHXs3OLTZqhflwmMC74wIpYDH252ECuGCw';
+const pass_email = '8EyFqd7QEFrp8Tv';
+const pass_mongodb = 'B2JpKgfFoMiEKQsz';
+const user_mongodb = 'kaiju00';
+module.exports = {
+    TELEGRAM_TOKEN,
+    pass_email,
+    pass_mongodb,
+    user_mongodb,
+
+}
